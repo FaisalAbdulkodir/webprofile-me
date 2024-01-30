e6027587b418d67e2bd334e4e9de810561cc643b
FaisalAbdulkodir <faisalabdullah168@gmail.com>
2024-01-30 23:18:41 +0700

    revert it back lol

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Buku from "@/assets/buku.png";
 import Sag from "@/assets/sag.png";
 import Emosi from "@/assets/emo.jpg";
-import React from "react";
+import React, { useState } from "react";
 import {
   Modal,
   ModalContent,
@@ -16,7 +16,6 @@ import {
 } from "@nextui-org/react";
 
 const Projects = () => {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
     <div className="pt-40">
       <h3 className="text-center text-2xl md:text-4xl font-bold text-color-accent ">
@@ -24,10 +23,7 @@ const Projects = () => {
       </h3>
       <div className="flex pt-20 justify-center">
         <div className="grid md:grid-cols-3 grid-cols-1 gap-4 px-4 text-center">
-          <div
-            onClick={onOpen}
-            className="cursor-pointer w-72 h-96 bg-color-accent mx-auto overflow-hidden shadow-xl flex flex-col border-4 border-color-accent hover:scale-105 transition-all duration-300 ease-in-out"
-          >
+          <div className="cursor-pointer w-72 h-96 bg-color-accent mx-auto overflow-hidden shadow-xl flex flex-col border-4 border-color-accent hover:scale-105 transition-all duration-300 ease-in-out">
             <div className="h-4/5 w-full flex-1 bg-color-primary">
               <Image
                 src={Buku}
@@ -41,49 +37,6 @@ const Projects = () => {
               <h1 className="font-bold text-lg md:text-xl text-color-dark h-full p-3">
                 Dynamic Correlation using GARCH Model
               </h1>
-            </div>
-            <div>
-              <Modal
-                backdrop="blur"
-                isOpen={isOpen}
-                onOpenChange={onOpenChange}
-                classNames={{
-                  backdrop:
-                    "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20",
-                }}
-              >
-                <ModalContent>
-                  {(onClose) => (
-                    <>
-                      <ModalHeader className="flex flex-col gap-1">
-                        Analisis Hubungan Harga Bahan Bakar Terhadap Bahan
-                        Pangan Menggunakan Generalized Autoregressive
-                        Conditional Heteroscedasticity(GARCH)
-                      </ModalHeader>
-                      <ModalBody>
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit. Nullam pulvinar risus non risus hendrerit
-                          venenatis. Pellentesque sit amet hendrerit risus, sed
-                          porttitor quam.
-                        </p>
-                      </ModalBody>
-                      <ModalFooter>
-                        <Button
-                          color="danger"
-                          variant="light"
-                          onPress={onClose}
-                        >
-                          Close
-                        </Button>
-                        <Button color="primary" onPress={onClose}>
-                          Action
-                        </Button>
-                      </ModalFooter>
-                    </>
-                  )}
-                </ModalContent>
-              </Modal>
             </div>
           </div>
           <div className="cursor-pointer w-72 h-96 bg-color-accent mx-auto overflow-hidden shadow-xl flex flex-col border-4 border-color-accent hover:scale-105 transition-all duration-300 ease-in-out">
--- src/components/Projects/tes.jsx
+++ src/components/Projects/tes.jsx
@@ -1,5 +1,10 @@
+"use client";
+
 import Image from "next/image";
-import React, { useState } from "react";
+import Buku from "@/assets/buku.png";
+import Sag from "@/assets/sag.png";
+import Emosi from "@/assets/emo.jpg";
+import React from "react";
 import {
   Modal,
   ModalContent,
@@ -10,61 +15,8 @@ import {
   useDisclosure,
 } from "@nextui-org/react";
 
-const ProjectItem = ({ imageSrc, title, description }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const openModal = () => {
-    onOpen();
-  };
-
-  return (
-    <div
-      className="cursor-pointer w-72 h-96 bg-color-accent mx-auto overflow-hidden shadow-xl flex flex-col border-4 border-color-accent hover:scale-105 transition-all duration-300 ease-in-out"
-      onClick={openModal}
-    >
-      <div className="h-4/5 w-full flex-1 bg-color-primary">
-        <Image
-          src={imageSrc}
-          alt="..."
-          width={250}
-          height={250}
-          className="object-cover h-full w-full object-top"
-        />
-      </div>
-      <div className="h-1/5 w-full">
-        <h1 className="font-bold text-lg md:text-xl text-color-dark h-full p-3">
-          {title}
-        </h1>
-      </div>
-      <Modal
-        backdrop="blur"
-        isOpen={isOpen}
-        onOpenChange={onClose}
-        classNames={{
-          backdrop:
-            "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20",
-        }}
-      >
-        <ModalContent>
-          <ModalHeader className="flex flex-col gap-1">{title}</ModalHeader>
-          <ModalBody>
-            <p>{description}</p>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="danger" variant="light" onPress={onClose}>
-              Close
-            </Button>
-            <Button color="primary" onPress={onClose}>
-              Action
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </div>
-  );
-};
-
 const Projects = () => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
     <div className="pt-40">
       <h3 className="text-center text-2xl md:text-4xl font-bold text-color-accent ">
@@ -72,25 +24,103 @@ const Projects = () => {
       </h3>
       <div className="flex pt-20 justify-center">
         <div className="grid md:grid-cols-3 grid-cols-1 gap-4 px-4 text-center">
-          <ProjectItem
-            imageSrc={Buku}
-            title="Dynamic Correlation using GARCH Model"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor quam."
-          />
-          <ProjectItem
-            imageSrc={Emosi}
-            title="Emotion Detection on Text Using Ensemble Model"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor quam."
-          />
-          <ProjectItem
-            imageSrc={Sag}
-            title="UI & UX Improvements on Dikpora Dompu Website"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor quam."
-          />
+          <div
+            onClick={onOpen}
+            className="cursor-pointer w-72 h-96 bg-color-accent mx-auto overflow-hidden shadow-xl flex flex-col border-4 border-color-accent hover:scale-105 transition-all duration-300 ease-in-out"
+          >
+            <div className="h-4/5 w-full flex-1 bg-color-primary">
+              <Image
+                src={Buku}
+                alt="..."
+                width={250}
+                height={250}
+                className="object-cover h-full w-full object-top"
+              />
+            </div>
+            <div className="h-1/5 w-full">
+              <h1 className="font-bold text-lg md:text-xl text-color-dark h-full p-3">
+                Dynamic Correlation using GARCH Model
+              </h1>
+            </div>
+            <div>
+              <Modal
+                backdrop="blur"
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}
+                classNames={{
+                  backdrop:
+                    "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20",
+                }}
+              >
+                <ModalContent>
+                  {(onClose) => (
+                    <>
+                      <ModalHeader className="flex flex-col gap-1">
+                        Analisis Hubungan Harga Bahan Bakar Terhadap Bahan
+                        Pangan Menggunakan Generalized Autoregressive
+                        Conditional Heteroscedasticity(GARCH)
+                      </ModalHeader>
+                      <ModalBody>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Nullam pulvinar risus non risus hendrerit
+                          venenatis. Pellentesque sit amet hendrerit risus, sed
+                          porttitor quam.
+                        </p>
+                      </ModalBody>
+                      <ModalFooter>
+                        <Button
+                          color="danger"
+                          variant="light"
+                          onPress={onClose}
+                        >
+                          Close
+                        </Button>
+                        <Button color="primary" onPress={onClose}>
+                          Action
+                        </Button>
+                      </ModalFooter>
+                    </>
+                  )}
+                </ModalContent>
+              </Modal>
+            </div>
+          </div>
+          <div className="cursor-pointer w-72 h-96 bg-color-accent mx-auto overflow-hidden shadow-xl flex flex-col border-4 border-color-accent hover:scale-105 transition-all duration-300 ease-in-out">
+            <div className="h-4/5 w-full flex-1 bg-color-primary">
+              <Image
+                src={Emosi}
+                alt="..."
+                width={250}
+                height={250}
+                className="object-cover h-full w-full object-top"
+              />
+            </div>
+            <div className="h-1/5 w-full">
+              <h1 className="font-bold text-lg md:text-xl text-color-dark h-full p-3">
+                Emotion Detection on Text Using Ensemble Model
+              </h1>
+            </div>
+          </div>
+          <div className="cursor-pointer w-72 h-96 bg-color-accent mx-auto overflow-hidden shadow-xl flex flex-col border-4 border-color-accent hover:scale-105 transition-all duration-300 ease-in-out">
+            <div className="h-4/5 w-full flex-1 bg-color-primary">
+              <Image
+                src={Sag}
+                alt="..."
+                width={250}
+                height={250}
+                className="object-cover h-full w-full object-top"
+              />
+            </div>
+            <div className="h-1/5 w-full">
+              <h1 className="font-bold text-lg md:text-xl text-color-dark h-full p-3">
+                UI & UX Improvements on Dikpora Dompu Website
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default Projects;
